079100e487e44290b4942e20b19cf92ab44278aa
0x1 <kevin.francart3@gmail.com>
2026-08-09 01:32:03 +0200

    misc

--- test/Test/Main.go
+++ test/Test/Main.go
@@ -1,8 +1,15 @@
 package Test_Main
 
 import (
+	"gopurs/output/Data.JSDate"
+	"gopurs/output/Foreign"
 	"gopurs/output/gopurs_runtime"
 	"time"
+	"fmt"
 )
 
-var MyDate = gopurs_runtime.Box(time.Now())
+var MyDate = gopurs_runtime.Box(Data_JSDate.Date{Ms: float64(time.Now().UnixNano()) / 1e6})
+
+func init() {
+	fmt.Println("TagOf MyDate:", Foreign.TagOf(MyDate).StrVal())
+}
